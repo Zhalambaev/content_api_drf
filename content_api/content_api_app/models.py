70b64f0d2615b97e98dbc9d09cc9db2867fb59ca
Denis Zhalambaev <cnchelper@yandex.ru>
2025-08-25 18:46:13 +0700

    models fin

--- content_api/content_api_app/models.py
+++ content_api/content_api_app/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class Page(models.Model):
+    title = models.CharField(max_length=250, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+
+class BaseContent(models.Model):
+    title = models.CharField(max_length=250, db_index=True)
+    counter = models.PositiveBigIntegerField(default=0)
+    order_id = models.PositiveIntegerField(default=0, db_index=True)
+
+    class Meta:
+        abstract = True
+
+
+class Video(BaseContent):
+    page = models.ForeignKey(
+        Page, on_delete=models.CASCADE, related_name='videos'
+    )
+    video_url = models.URLField()
+    subtitles_url = models.URLField(blank=True)
+
+    def __str__(self):
+        return f'Video: {self.title}'
+
+
+class Audio(BaseContent):
+    """В ТЗ ссылки хранения нет. Но хранить где-то нужно.
+    При реальной работе уточнил бы этот момент.
+    В тестовом решил добавить audio_url.
+    """
+    page = models.ForeignKey(
+        Page, on_delete=models.CASCADE, related_name='audios'
+    )
+    audio_url = models.URLField()
+    text = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'Audio: {self.title}'
