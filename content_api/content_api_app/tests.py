b2fdd61f70c2669f8d4456c2fbcd5557d426ec38
Zhalambaev <cnchelper@yandex.ru>
2025-08-27 08:53:27 +0700

    test update

--- content_api/content_api_app/tests.py
+++ content_api/content_api_app/tests.py
@@ -7,92 +7,104 @@ from .models import Page, Video, Audio
 
 
 class PagesListApiTest(TestCase):
-
     def setUp(self):
         self.client = APIClient()
-        # создаём 12 страниц, чтобы проверить пагинацию (page_size=10)
         for i in range(1, 13):
             Page.objects.create(title=f'page_{i}')
 
-    def test_pages_list_pagination_and_detail_url(self):
-        url = reverse('page_list')
-        resp = self.client.get(url)
+    def test_status_ok(self):
+        resp = self.client.get(reverse('page_list'))
         self.assertEqual(resp.status_code, 200)
 
-        data = resp.json()
-        # Проверяем структуру ответа DRF с пагинацией
-        self.assertIn('count', data)
-        self.assertIn('next', data)
-        self.assertIn('previous', data)
-        self.assertIn('results', data)
+    def test_response_has_pagination_keys(self):
+        data = self.client.get(reverse('page_list')).json()
+        for key in ('count', 'next', 'previous', 'results'):
+            self.assertIn(key, data)
 
-        # На первой странице должно быть 10 объектов (PagePagination.page_size)
+    def test_default_page_size_is_10(self):
+        data = self.client.get(reverse('page_list')).json()
         self.assertEqual(len(data['results']), 10)
 
-        # У каждого элемента есть id, title, detail_url
+    def test_detail_url_present_and_correct(self):
+        data = self.client.get(reverse('page_list')).json()
         item = data['results'][0]
         self.assertIn('id', item)
         self.assertIn('title', item)
         self.assertIn('detail_url', item)
         self.assertTrue(item['detail_url'].endswith(f'/api/pages/{item["id"]}/'))
 
-        # Проверяем, что можно изменить размер страницы через ?page_size=5
-        resp2 = self.client.get(url + '?page_size=5')
-        data2 = resp2.json()
-        self.assertEqual(len(data2['results']), 5)
+    def test_page_size_query_param_overrides_default(self):
+        data = self.client.get(reverse('page_list') + '?page_size=5').json()
+        self.assertEqual(len(data['results']), 5)
 
 
 class PageDetailApiTest(TestCase):
-
     def setUp(self):
         self.client = APIClient()
-        # Страница
         self.page = Page.objects.create(title='detail_page')
-        # Контент: два видео и одно аудио
-        self.v1 = Video.objects.create(
-            page=self.page, title='Video_1',
+
+        Video.objects.create(
+            page=self.page,
+            title='Video_1',
             order_id=2,
             video_url='http://video.com/1/',
             subtitles_url='http://video.com/1/subtitles/'
         )
-        self.v2 = Video.objects.create(
+        Video.objects.create(
             page=self.page,
             title='Video_2',
             order_id=1,
             video_url='http://video.com/2/'
         )
-        self.a1 = Audio.objects.create(
+        Audio.objects.create(
             page=self.page,
             title='Audio_1',
             order_id=3,
-            audio_url='	http://audio.com/1/',
+            audio_url='http://audio.com/1/',
             text='audio_text_1'
         )
 
-    @patch('content_api_app.tasks.counters_for_page.delay')
-    def test_page_detail_returns_full_sorted_contents_and_triggers_celery(self, mock_delay):
-        url = reverse('page_detail', args=[self.page.id]) 
-        resp = self.client.get(url)
+    def _get_detail(self):
+        url = reverse('page_detail', args=[self.page.id])
+        return self.client.get(url).json()
+
+    @patch('content_api_app.views.counters_for_page.delay')
+    def test_status_ok(self, _mock_delay):
+        resp = self.client.get(reverse('page_detail', args=[self.page.id]))
         self.assertEqual(resp.status_code, 200)
 
-        data = resp.json()
-        # Базовые поля страницы
+    @patch('content_api_app.views.counters_for_page.delay')
+    def test_base_fields_present(self, _mock_delay):
+        data = self._get_detail()
         self.assertEqual(data['id'], self.page.id)
         self.assertEqual(data['title'], self.page.title)
         self.assertIn('contents', data)
 
-        contents = data['contents']
-        # Должны быть все 3 элемента
-        self.assertEqual(len(contents), 3)
+    @patch('content_api_app.views.counters_for_page.delay')
+    def test_contents_length_is_3(self, _mock_delay):
+        data = self._get_detail()
+        self.assertEqual(len(data['contents']), 3)
 
-        # Проверяем сортировку по order_id
-        order_ids = [c['order_id'] for c in contents]
+    @patch('content_api_app.views.counters_for_page.delay')
+    def test_contents_sorted_by_order_id(self, _mock_delay):
+        data = self._get_detail()
+        order_ids = [c['order_id'] for c in data['contents']]
         self.assertEqual(order_ids, [1, 2, 3])
 
-        # Видео содержит video_url
-        self.assertIn('video_url', contents[0]['content'])
-        # Аудио содержит audio_url
-        self.assertIn('audio_url', contents[-1]['content'])
+    @patch('content_api_app.views.counters_for_page.delay')
+    def test_first_item_has_video_specific_fields(self, _mock_delay):
+        data = self._get_detail()
+        first = data['contents'][0]['content']
+        self.assertIn('video_url', first)
 
-        # Celery-таска вызвана
+    @patch('content_api_app.views.counters_for_page.delay')
+    def test_last_item_has_audio_specific_fields(self, _mock_delay):
+        data = self._get_detail()
+        last = data['contents'][-1]['content']
+        self.assertIn('audio_url', last)
+        self.assertIn('text', last)
+
+    @patch('content_api_app.views.counters_for_page.delay')
+    def test_celery_task_is_triggered(self, mock_delay):
+        self.client.get(reverse('page_detail', args=[self.page.id]))
         mock_delay.assert_called_once_with(self.page.id)
